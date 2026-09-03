0a395014cfe941c841370c638f3704bda4037868
srinu <medisettisrinu2004@gmail.com>
2026-09-03 11:53:13 +0530

    adding route to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,13 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.status(200).send('Server is running');
 });
-
+app.get("/products", (req, res) => {
+  res.status(200).json([
+    { id: 1, name: "Product 1", price: 10.0 },
+    { id: 2, name: "Product 2", price: 20.0 },
+    { id: 3, name: "Product 3", price: 30.0 },
+  ]);
+});
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
